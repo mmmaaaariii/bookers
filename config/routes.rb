3d77ab767215269a9fd3f2f16868d385acef8838
mmmaaaariii <166019982+mmmaaaariii@users.noreply.github.com>
2024-05-19 13:04:59 +0000

    [Update]ファイルの記述・CRUDの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :users
   get '/', to: 'homes#top'
-  # get 'books/new'
-  # get 'books/index'
-  # get 'books/show'
-  # get 'books/edit'
+  get 'books' => 'books#index'
+  post 'books' => 'books#create' 
+  get 'books/:id' => 'books#show'
+  get 'books/:id/edit' => 'books#edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
