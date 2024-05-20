834b745d1d5a2da4d0b7803f9fe7abd62d9bcfb5
mmmaaaariii <166019982+mmmaaaariii@users.noreply.github.com>
2024-05-20 14:36:06 +0000

    [Update]indexとshowのボタン

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to 'books'
+    redirect_to book_path(book.id)
   end
 
   def show
@@ -19,6 +19,12 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)  
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   resources :users
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/', to: 'homes#top'
-  get 'books' => 'books#index'
+  get 'books' => 'books#index', as: 'index'
   post 'books' => 'books#create' 
-  get 'books/:id' => 'books#show'
-  get 'books/:id/edit' => 'books#edit'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+　patch 'books/:id' => 'books#update', as: 'update_book'
 end
